9afe0d3977e2bd552b3ddc3bfd732afd162b2e02
Chazni Katz <Chazni.k9@gmail.com>
2023-04-11 01:13:17 +0200

    scoring logic done, needs clean up

--- quizgame/main.go
+++ quizgame/main.go
@@ -3,13 +3,34 @@ package main
 import  (
         "flag"
         "fmt"
-        "io"
+        "os"
+        "bufio"
+        "log"
+        "strings"
         )
 
 func main() {
     initialiseFlags()
     fileName := "problems.csv"
     quizGame := readFile(fileName)
+    totalQuestions := len(quizGame)
+    score := 0
+
+    reader := bufio.NewReader(os.Stdin) 
+
+    for _, qAndA := range quizGame {
+        fmt.Print(qAndA.question + " = ")
+        
+        ans, err := reader.ReadString('\n')
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        if strings.Trim(ans, " \n") == strings.Trim(qAndA.answer, " \n") {
+            score++
+        }
+    }
+    fmt.Printf("You score %d out of %d", score, totalQuestions)
 }
 
 
